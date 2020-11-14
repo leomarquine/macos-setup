a614cd12e64cd1eb735a732d86f1bf269b4061e3
Leonardo Marquine <leonardomarquine@gmail.com>
2020-11-14 18:39:44 -0300

    Install Xcode Command Line Tools

--- setup.sh
+++ setup.sh
@@ -1,1 +1,5 @@
 #!/usr/bin/env bash
+
+# Xcode Command Line Tools
+touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress
+softwareupdate --install "$(softwareupdate --list | sed -n -e 's/^.*\(Command Line Tools\)/\1/p' | head -1)"
