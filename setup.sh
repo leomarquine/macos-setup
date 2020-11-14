29a6ade1855cca5433f672789591d2a97c57bef9
Leonardo Marquine <leonardomarquine@gmail.com>
2020-11-14 18:57:19 -0300

    Install Homebrew

--- setup.sh
+++ setup.sh
@@ -6,3 +6,12 @@ if test ! $(xcode-select --print-path); then
     softwareupdate --install "$(softwareupdate --list | sed -n -e 's/^.*\(Command Line Tools\)/\1/p' | head -1)"
     rm /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress
 fi
+
+
+# Homebrew
+if ! hash brew 2>/dev/null; then
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+fi
+
+# Update Homebrew recipes
+brew update
